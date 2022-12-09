7caf1cbe5c2065c1cffd2d79894f797c9a8dc130
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2022-12-09 14:11:09 +0000

    Added BrowserRouter and initial page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import Home from "./pages/home.jsx";
+import Header from "./components/header.jsx";
+import Footer from "./components/footer.jsx"
+import NoPage from "./pages/nopage.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 export default function App() {
   return (
-    <main>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
       
-      mystyle React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
+          <Route index element={<Home />} />
+          
+          <Route path="*" element={<NoPage />} />
+        
+        
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
